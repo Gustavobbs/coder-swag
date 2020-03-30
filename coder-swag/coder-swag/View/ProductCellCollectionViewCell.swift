afc26e2d606a637eff4f12b8987c190422c67afd
Gustavobbs <36171903+Gustavobbs@users.noreply.github.com>
2020-03-30 14:51:21 -0300

    UICollectionView added

---
+++ coder-swag/coder-swag/View/ProductCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Gustavo Buoro Branco de Souza on 30/03/20.
+//  Copyright © 2020 Gustavo Buoro Branco de Souza. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImage!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
